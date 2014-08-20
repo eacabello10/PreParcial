ff3c277d4d4ab26f69a74a68e25337cc4b153278
eacabello10 <ea.cabello10@uniandes.edu.co>
2014-08-20 10:31:21 -0500

    agregue una o

--- sportclub.web/src/main/webapp/src/controller/_countryController.js
+++ sportclub.web/src/main/webapp/src/controller/_countryController.js
@@ -173,7 +173,7 @@ define(['model/countryModel'], function(countryModel) {
         },
         
         language: function(){
-            alert('Usted esta viendo la version en ESPANOL de esta paginaa');
+            alert('Usted esta viendo la version en ESPANOL de esta paginao');
         }
         
     });
